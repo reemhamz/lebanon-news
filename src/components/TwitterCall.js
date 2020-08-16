8d58c063ad797dcf9816bf7a256ac23a7d46ce38
Reem H <reem@fueled.com>
2020-08-15 23:09:22 -0400

    testststs

---
+++ src/components/TwitterCall.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
+function TwitterCall() {
+    const [twitterHead, setRedditHead] = useState('');
+
+        return (
+            <div className="App">
+                    test
+            </div>
+        );
+    
+}
+
+export default TwitterCall;
