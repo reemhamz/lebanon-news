afff8407c3d51a8f6628b0424881ea53050bdf00
Reem H <reem@fueled.com>
2020-08-23 23:44:43 -0400

    Twitters hates CORS apparantly???

--- src/components/TwitterCall.js
+++ src/components/TwitterCall.js
@@ -1,34 +1,42 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import {API_KEY, API_SECRET_KEY, BEARER_TOKEN} from "../config"
-
+import { API_KEY, API_SECRET_KEY, BEARER_TOKEN } from "../config";
 
 function TwitterCall() {
-    const [twitterHead, setTwitterHead] = useState('');
-    const apiKey = API_KEY;
-    const secretKey = API_SECRET_KEY;
-    const bearerToken = BEARER_TOKEN;
-    
-    const twitterURL = "https://api.twitter.com/1.1/tweets/search/fullarchive/beirut.json"  
-    
-    // Twitter HTTP request
-    useEffect(() => {
-      axios({
-        method: "POST",
-        url: twitterURL,
-      }).then((res) => {
-        console.log(res.data.data)
-        
-      });
-    }, []);
+  const Twitter = require("twitter");
 
-    return (
-            <div className="App">
-                
-            </div>
-        );
-    
+  const [twitterHead, setTwitterHead] = useState("");
+  const apiKey = API_KEY;
+  const secretKey = API_SECRET_KEY;
+  const bearerToken = BEARER_TOKEN;
+
+  const twitterURL = "https://api.twitter.com/2/tweets";
+  const client = new Twitter({
+    consumer_key: apiKey,
+    consumer_secret: secretKey,
+    bearer_token: bearerToken,
+  });
+
+  // Twitter HTTP request
+  useEffect(() => {
+    console.log('this is it', client.bearer_token)
+    axios({
+      method: "GET",
+      url: twitterURL,
+      dataType: "json",
+      headers: {
+        Authorization: `${client.bearerToken}`,
+      },
+      params: {
+        q: 'beirut'
+      }
+    }).then((res) => {
+      console.log(res.data.data);
+    });
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default TwitterCall;
